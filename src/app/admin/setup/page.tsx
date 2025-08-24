01eba49dbec1340b9a534fe224f1e3fc09b6be88
Walid-EM <cronosano@gmail.coom>
2025-08-24 08:15:48 +0200

    .env corriger

--- src/app/admin/setup/page.tsx
+++ src/app/admin/setup/page.tsx
@@ -172,10 +172,10 @@ export default function AdminSetup() {
 
         <div className="text-center">
           <p className="text-gray-400 text-sm">
-            ⚠️ Cette page ne sera accessible qu&apos;une seule fois
+            ⚠️ Cette page ne sera accessible qu&apos;une seule fois.
           </p>
           <p className="text-gray-500 text-xs mt-1">
-            Après la création, utilisez la page de connexion
+            Après la création, utilisez la page de connexion.
           </p>
         </div>
       </div>
